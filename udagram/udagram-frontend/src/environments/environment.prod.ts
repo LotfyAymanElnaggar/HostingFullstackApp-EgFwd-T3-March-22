09431388f6d21333f72cd251ea3521c6b61198bd
LotfyAymanElnaggar <lotfyaymanelnaggar@gmail.com>
2022-06-23 20:14:38 +0200

    .

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: 'Udagram',
-  apiHost: 'http://tifanaga.eba-afzir7tp.us-east-1.elasticbeanstalk.com/api/v0',
+  appName: "Udagram",
+  // apiHost: 'http://tifanaga.eba-afzir7tp.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
