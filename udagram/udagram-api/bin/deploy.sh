af3bcc9c44164d1dbba76542cf86ef53a24acfd3
LotfyAymanElnaggar <lotfyaymanelnaggar@gmail.com>
2022-06-23 07:51:07 +0200

    .

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -9,6 +9,6 @@ eb use tifanaga --profile fullstackuser
 eb deploy --profile fullstackuser
 eb setenv POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PORT=$POSTGRES_PORT POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB JWT_SECRET=$JWT_SECRET PORT=$PORT AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL --profile fullstackuser
 
-echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
+echo "[profile JJJJJJJJJJJJJ]"                              > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
