b263100295b18088ccf579fbc4bca800f8a6a554
LotfyAymanElnaggar <lotfyaymanelnaggar@gmail.com>
2022-06-25 06:39:55 +0200

    resubmit

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0',
+  production: true,
+  appName: "Udagram",
+  apiHost: "http://tifanaga.eba-afzir7tp.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
